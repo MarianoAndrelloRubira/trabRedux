e2727959899d05d5d86f17978bb7673ffa6c7929
MarianoAndrelloRubira <mariano_rubira2020@hotmail.com>
2023-09-11 09:52:24 -0300

    feat:validação do fomCadCliente e estado

--- src/templates/Menu.jsx
+++ src/templates/Menu.jsx
@@ -6,6 +6,7 @@ export default function Menu(props) {
         <Navbar expand="lg" className="bg-body-tertiary">
             <Container>
                 <Navbar.Brand as={Link} to="/">Menu</Navbar.Brand>
+            
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
